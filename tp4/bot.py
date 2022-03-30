f8509732d6fd8c31662e00a08125ee86b4ed0cee
Lancelot <journel@et.esiea.fr>
2022-03-30 13:35:12 +0200

    debut tp4, reponse aux questions basique

---
+++ tp4/bot.py
@@ -0,0 +1,2 @@
+# Un bot est un logiciel informatique qui est créé à partir d'une application et qui peut être vu comme un utilisateur à part entière
+# Le logging est la gestion des logs, et les logs sont les fichiers qui vont garder un historique des activités des événements
