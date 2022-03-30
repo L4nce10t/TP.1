a1679b41039afb6df24a1ef247ae489de1cc57f5
Lancelot <journel@et.esiea.fr>
2022-03-30 15:21:24 +0200

    debut tp4, reponse du bot quand on dit Ping

--- tp4/bot.py
+++ tp4/bot.py
@@ -1,2 +1,23 @@
 # Un bot est un logiciel informatique qui est créé à partir d'une application et qui peut être vu comme un utilisateur à part entière
 # Le logging est la gestion des logs, et les logs sont les fichiers qui vont garder un historique des activités des événements
+
+from discord import Client
+import discord
+
+
+class MyBot(Client):
+    def __init__(self):
+        super().__init__()
+        self.run("OTU4NjkxMzU3NzYzOTg1NDQ4.YkRA5w.cZbbPlnBSfvTHjilWINvEwq2pbE")
+
+   #
+    # Créer un événement
+    async def on_ready(self):
+        print("Le bot est prêt !")
+
+    async def on_message(ctx, message):
+        if message.content == "Ping":
+            await message.channel.send("Pong")
+
+
+bot = MyBot()  # instance = appel de fonction
