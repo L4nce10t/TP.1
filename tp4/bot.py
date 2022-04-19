8d4221c3d2344feb248186a654900b7b1e666a5f
Lancelot <journel@et.esiea.fr>
2022-04-19 11:51:41 +0200

    ajout de la fonction pour l'arrivé d'un membre

--- tp4/bot.py
+++ tp4/bot.py
@@ -1,6 +1,7 @@
 # Un bot est un logiciel informatique qui est créé à partir d'une application et qui peut être vu comme un utilisateur à part entière
 # Le logging est la gestion des logs, et les logs sont les fichiers qui vont garder un historique des activités des événements
 
+from distutils.cmd import Command
 from email.policy import default
 from discord import Client
 import discord
@@ -10,21 +11,35 @@ import os
 
 from dotenv import load_dotenv
 
+load_dotenv(dotenv_path="config")
+
 
 class MyBot(Client):
-    def __init__(self):
-        super().__init__()
-        self.run("OTU4NjkxMzU3NzYzOTg1NDQ4.YkRA5w.UT2_PfC1jXsVOurAmcINN1j2QCE")
 
-   #
+    def __init__(self):
+
+        intents = discord.Intents.default()
+        intents.presences = True
+        intents.members = True
+        super().__init__(command_prefix="!", intents=intents)
     # Créer un événement
+
     async def on_ready(self):
         print("Le bot est prêt !")
+        print(f"{self.user.display_name} est connecté au serveur")
 
     async def on_message(ctx, message):
         if message.content == "Ping":
-            await message.channel.send("Pong") #commentaire
+            await message.channel.send("Pong")  # commentaire
+
+    # Reagi quand les membres qui arrivent et souhaite bienvenue aux nouveaux arrivants
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = 'Salut le jeuns {0.mention} dans {1.name}!'.format(
+                member, guild)
+            await guild.system_channel.send(to_send)
 
 
-default_intents = discord.Intents.default()
 bot = MyBot()  # instance = appel de fonction
+bot.run(os.getenv("TOKEN"))
